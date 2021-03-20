54ef038f054e9885dae40196bf61b002692d6564
pandey00 <69048055+pandey00@users.noreply.github.com>
2021-03-20 10:13:16 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1798480103:AAGvCQN787sm9CrJ257zaawqeiJ3uecF_7U"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485393652
-owner_id = 1243703097
+sudo_chat_id = -1001452962072
+owner_id = 777252565
